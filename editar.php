0871f612bfd06a4d0e8a0051b3a493b394757711
Rodrigo Cardoso <diazrma@gmail.com>
2020-08-06 18:17:27 -0300

    feature/cover
    Adicionei o js pra pegar o id da foto que vai se tornar capa dentro do click
    tem um ajax que vai enviar para o php esse id
    Dentro desse php precisa fazer duas coisas atualizar o campo capa dessa imagem como 1 e as demais fotos como 0
    
    Tipo assim tem o carro audi com 3 fotos
    
    foto1 capa 1
    foto2 capa0
    foto3 capa0
    
    Cada vez que clicar no radio da foto ele salva o valor 1 no campo capa daquela foto e nas demais seta valor 0
    
    Assim vc consegue separar as imagens

--- editar.php
+++ editar.php
@@ -100,7 +100,7 @@ $fotos = $callClassVeiculo->showFotos($id);
                               <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
                             </svg>
                         </button>
-                        <input type="radio" id="situacao" name="situacao" class="situacao" value="0">
+                        <input type="radio" id="situacao" data-id="<?= $foto['cod_foto']?>" name="situacao" class="situacao" value="0">
                         <label>Definir foto inicial</label>
                     </div>
                 </div>
@@ -143,9 +143,23 @@ $fotos = $callClassVeiculo->showFotos($id);
             });
         });
 
-
+        //Função para marcar a foto como capa
+        //Envia esse id_foto pra um php que faz um update no campo capa com o valor 1 e as demais foto precisa colcoar valor 0
         $('.situacao').click(function(){
-          var valor = $(this).attr('value');
+                 
+          var id_foto=  $(this).attr("data-id");
+            console.log(id_foto)
+          $.ajax({
+                url: "arquivo.php",
+                type: 'POST',
+                data: {
+                    'id_foto': id_foto
+                }, success: function (data) {
+               
+                }
+            });
+          
+          
         });
     });
 </script>
